2281dc595d6c5c575e16d566676a64c6d3494438
okapgate <okapgate@gmail.com>
2018-11-24 05:07:27 -0800

    fetch images from upload folder

--- app.js
+++ app.js
@@ -32,6 +32,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use('/uploads',express.static(path.join(__dirname, 'uploads')));
 
 app.use(passport.initialize());
 require('./passport/passport')(passport);
