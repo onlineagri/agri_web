0e9504a3e1d7d1360689b2def73d1b31fcac0e6f
okapgate <okapgate@gmail.com>
2019-01-20 16:29:11 +0530

    contact email format

--- modules/userAuthentication/authenticationController.js
+++ modules/userAuthentication/authenticationController.js
@@ -346,15 +346,7 @@ exports.sendContactEmail = function(req, res){
         '    <td valign="top" align="left" style="border-collapse: collapse; border-spacing: 0; margin: 0; padding: 0; padding-left: 6.25%; padding-right: 6.25%; width: 87.5%; font-size: 17px; font-weight: 400; line-height: 160%;' +
         '    padding-top: 25px; ' +
         '   color: #000000;' +
-        '    font-family: sans-serif;">Dear  dealsTickTeam,<br/><br/>' + contactData.message + ' </td>' +
-        '   <td valign="top" align="left" style="border-collapse: collapse; border-spacing: 0; margin: 0; padding: 0; padding-left: 6.25%; padding-right: 6.25%; width: 87.5%; font-size: 17px; font-weight: 400; line-height: 160%;' +
-        '    padding-top: 25px; ' +
-        '   color: #000000;' +
-        '    font-family: sans-serif;">From,<br/><br/>' + contactData.name + ' </td>' +
-        '   <td valign="top" align="left" style="border-collapse: collapse; border-spacing: 0; margin: 0; padding: 0; padding-left: 6.25%; padding-right: 6.25%; width: 87.5%; font-size: 17px; font-weight: 400; line-height: 160%;' +
-        '    padding-top: 25px; ' +
-        '   color: #000000;' +
-        '    font-family: sans-serif;">Sender Mobile,<br/><br/>' + contactData.phoneNumber + ' </td>' +
+        '    font-family: sans-serif;">Dear  dealsTickTeam,<br/><br/>' + contactData.message + '<br/> Sender Name:'+ contactData.name +'<br/> Sender phoneNumber:'+contactData.phoneNumber+'</td>' +
         '   </tr>' +
         '<tr>' +
         '    <td valign="top" align="left" style="border-collapse: collapse; border-spacing: 0; margin: 0; padding: 0; padding-left: 6.25%; padding-right: 6.25%; width: 87.5%; font-size: 17px; font-weight: 400; line-height: 160%;' +
